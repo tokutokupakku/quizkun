3d5b00acbb6168959d520968a8b24a29f1da63eb
Yuka Tokunaga <yuka.tokunaga@di-v.co.jp>
2020-05-19 12:57:56 +0900

    作成途中

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,5 +1,14 @@
 $(function() {
-  $('answer_button').click(function() {
-    $(this).next().text('クリクありがとうございます！');
+  $('.answer_button').click(function(e) {
+    e.preventDefault();
+    
+    $.ajax({
+      url: url, //送信先
+      type:'POST', //送信方法
+      datatype: 'json', //受け取りデータの種類
+      data:{
+       'id' : $('#id_number').val()
+      }
+      })
   });
-})
+});
